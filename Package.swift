5d15d14d41ab4a0087b6ce91c11f659bd5cf9cc0
Brendan Thompson <me@brendanthompson.com>
2026-09-23 10:22:40 +1000

    fix: package swift file

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Test Package",
     products: [
+        .library(
             name: "TestPackage",
             targets: ["TestPackage"]
         ),
